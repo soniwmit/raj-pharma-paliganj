c820ab0b08b629f3708c4fcf560378cb71e470ee
soniwmit <soni.wmit@gmail.com>
2026-08-10 15:37:28 +0530

    Update useTracker.ts

--- src/hooks/useTracker.ts
+++ src/hooks/useTracker.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export function useTracker(currentPage?: string) {
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
